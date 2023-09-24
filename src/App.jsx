f09f6a941ead653fe939b76147a0629a7e1d36d0
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-09-24 16:19:44 +0530

    day 3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-function App(){
-return(
-  <div>
-    <p>Hello World!</p>
-  </div>
-)
+function App(Props) {
+
+  const { notes } = Props;
+  const addNote = (event)=>{
+    event.preventDefault();
+  console.log(event.target);
+
+  }
+  return (
+
+    <div>
+      <p>Hello World!</p>
+      <ul>
+        {
+          notes.map((note, index) => {
+            return <li key={index} >{note.content}</li>
+          })
+        }
+      </ul>
+
+      <form action="">
+        <input type="text" />
+        <button onClick={addNote}>Save Note</button>
+      </form>
+    </div>
+  )
 }
 
-export default App;
+export default App; 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+const notes = [
+    {
+        id:1,
+        content: 'Working with Lists',
+        important:true
+    },
+    {
+        id:2,
+        content:"Props Vs State",
+        important:false
+    },
+    {
+        id:3,
+        content:'Usage ofkeys',
+        important:false
+    },
+    {
+        id:4,
+        content:'Conditional Rendering',
+        important:true
+    },
+    {
+        id:5,
+        content:'Handling Events',
+        important:true
+    }
+]
+    
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    <App notes={notes}/>
   
 )
